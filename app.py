61d301195cbf12e591d8fc7a3c09acd4c2182e4d
emilia175 <sunshineedolphin@gmail.com>
2026-01-22 15:10:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,21 @@
-from flask import Flask, render_template, request, send_file
+from flask import Flask, render_template, Response
 from tapis import generate_tapis_png
-import io
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET"])
+@app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/image")
-def image():
-    top = request.args.get("top", "").upper()
-    side = request.args.get("side", "").upper()
-
-    if not top or not side:
-        return "Lipsesc datele", 400
-
-    img = generate_tapis_png(top, side)
-
-    buf = io.BytesIO()
-    img.save(buf, format="PNG")
-    buf.seek(0)
-
-    return send_file(buf, mimetype="image/png")
+@app.route("/tapis")
+def tapis():
+    png = generate_tapis_png("ABCDE", "FGHIJ")
+    return Response(png, mimetype="image/png")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
+
+
 
 
 
